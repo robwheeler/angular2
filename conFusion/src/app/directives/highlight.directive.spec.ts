2d55a52da5e56d270ba4f6f537ee6cde5c021296
Rob Wheeler <rob.wheeler@gmail.com>
2021-02-21 15:48:21 -0800

    End-to-end testing

--- conFusion/src/app/directives/highlight.directive.spec.ts
+++ conFusion/src/app/directives/highlight.directive.spec.ts
@@ -1,8 +1,12 @@
 import { HighlightDirective } from './highlight.directive';
+import {ElementRef, Renderer2} from '@angular/core';
+
+let elementRef: ElementRef;
+let rendere2: Renderer2;
 
 describe('HighlightDirective', () => {
-  //it('should create an instance', () => {
-    //const directive = new HighlightDirective();
-    //expect(directive).toBeTruthy();
-  //});
+  it('should create an instance', () => {
+    const directive = new HighlightDirective(elementRef, renderer2);
+    expect(directive).toBeTruthy();
+  });
 });
